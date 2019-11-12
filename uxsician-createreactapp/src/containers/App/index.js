2aa998a61f0e2f7e3c9ba80b3b131485fed53be7
Eric Marin <marin.boo@gmail.com>
2019-11-12 08:35:54 -0600

    creating folder for different boilerplates

---
+++ uxsician-createreactapp/src/containers/App/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <div>
+            <h1>React Boilerplate App</h1>
+            <p>
+                <span role="img" aria-label="Bolt">
+                    🛸
+                </span>{' '}
+                Humanazing Technology!{'  '}
+                <span role="img" aria-label="Bolt">
+                    🛸
+                </span>
+            </p>
+        </div>
+    </div>
+);
+
+export default App;
